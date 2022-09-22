4f3e6c40c243fe0e8a6c99c456a900a7b19539ea
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-09-21 23:46:46 -0300

    Requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class Mission extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <>
+        <div data-testid="missions" />
+        <Title headline="Missões" />
+      </>
     );
   }
 }
